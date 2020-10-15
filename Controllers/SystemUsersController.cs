0f7cbaebbe289e2a9494d0bb130e719b62635478
g4genious <g_4_genious@yahoo.com>
2020-10-15 19:49:36 +0500

    user registration flow updated.

--- Controllers/SystemUsersController.cs
+++ Controllers/SystemUsersController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -12,10 +16,12 @@ namespace ThetaPOS.Controllers
     public class SystemUsersController : Controller
     {
         private readonly theta_posContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public SystemUsersController(theta_posContext context)
+        public SystemUsersController(theta_posContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
 
         // GET: SystemUsers
@@ -53,12 +59,62 @@ namespace ThetaPOS.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Username,Password,DisplayName,ProfilePicture,Address,Mobile,Role,Email,Status,CreatedDate,CreatedBy,ModifiedDate,ModifiedBy")] SystemUser systemUser)
+        public async Task<IActionResult> Create( SystemUser systemUser, IFormFile PP)
         {
             if (ModelState.IsValid)
             {
+                if(PP!=null && PP.Length>0)
+                {
+                    string ppsPath = _env.WebRootPath + "/pps/";
+                    string FileUniqueName = Guid.NewGuid().ToString()+Path.GetExtension(PP.FileName);
+                  await  PP.CopyToAsync(new FileStream(ppsPath+FileUniqueName,FileMode.CreateNew));
+
+                    systemUser.ProfilePicture = FileUniqueName;
+                }
+
+
+                systemUser.Role = "Staff";
+                systemUser.CreatedDate = DateTime.Now;
+                systemUser.CreatedBy = "System";
+
+
                 _context.Add(systemUser);
                 await _context.SaveChangesAsync();
+
+                //send registration email to new user
+
+                if (!string.IsNullOrEmpty(systemUser.Email))
+                {
+                    MailMessage MM = new MailMessage();
+                    MM.From = new MailAddress("students.thetasolutions@gmail.com", "Theta POS");
+                    MM.To.Add(systemUser.Email);
+                    MM.Subject = "Welcome to Theta POS";
+                    MM.Body = "Dear " + systemUser.DisplayName + ",<br/><br/>Thanks for registering with Theta POS. Please find below your credentials and keep them safe to login on Theta POS.<br/><br/>" +
+
+                        "<span style='color:green;'> Username: " + systemUser.Username + "<br/>" +
+                        "Password: " + systemUser.Password + "</span><br/><br/>" +
+
+                        "Feel free to contact us in case you need any assistance.<br/><br/>" +
+
+                        "Regards,<br/>" +
+                        "Team Theta POS";
+
+                    MM.IsBodyHtml = true;
+
+
+
+
+                    SmtpClient SC = new SmtpClient();
+                    SC.Credentials = new System.Net.NetworkCredential("students.thetasolutions@gmail.com", "fBd6LHc3zX2t");
+                    SC.Host = "smtp.gmail.com";
+                    SC.Port = 587;
+                    SC.EnableSsl = true;
+                   await SC.SendMailAsync(MM);
+
+
+                }
+                
+                
                 return RedirectToAction(nameof(Index));
             }
             return View(systemUser);
