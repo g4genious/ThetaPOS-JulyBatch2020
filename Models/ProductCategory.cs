92cd8bbd963ef835a8ae501777b91feed3862fe5
Shahzad <fl_shahzad@outlook.com>
2020-10-19 23:36:29 -0700

    using dataAnnotations in Models and Completeing flows of Category Check and Delete Check.

--- Models/ProductCategory.cs
+++ Models/ProductCategory.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ThetaPOS.Models
 {
     public partial class ProductCategory
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(500)]
+       
         public string Description { get; set; }
+        [Required]
         public string Image { get; set; }
+     
         public string ParentCategory { get; set; }
         public string Status { get; set; }
         public DateTime? CreatedDate { get; set; }
